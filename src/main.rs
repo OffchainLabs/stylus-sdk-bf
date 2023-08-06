7c0780ef0a4a8a04d7e30bff514f8a9db684be1a
Rachel Bousfield <nfranks@protonmail.com>
2023-08-05 22:11:36 -0600

    ready for crates.io

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+// Copyright 2022-2023, Offchain Labs, Inc.
+// For licensing, see https://github.com/OffchainLabs/stylus-sdk-bf/blob/stylus/licenses/COPYRIGHT.md
+
+fn main() {
+    println!("Hello, Stylus!");
+}
